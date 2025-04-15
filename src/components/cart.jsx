bd3ec3de2cf25c29cb67f97984e4ef03bac53340
ibr0him77 <your-email@associated-with-ibr0him77.com>
2025-04-15 19:37:48 +0500

    ozgarishh

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -25,7 +25,9 @@ const Cart = ({ cart, updateQuantity, toggleCart, clearCart }) => {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 50 }}
       transition={{ duration: 0.3 }}
-      className="fixed bottom-0 inset-x-0 md:top-16 md:bottom-auto md:right-0 md:left-auto md:w-96 w-full bg-white shadow-lg z-50 p-6 rounded-t-2xl md:rounded-b-lg"
+      className="fixed z-50 p-6 bg-white shadow-lg w-full md:w-96 rounded-t-2xl md:rounded-lg
+                 bottom-0 left-0 right-0 
+                 md:bottom-auto md:top-20 md:right-4 md:left-auto"
     >
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-bold text-black">Your Cart</h2>
@@ -134,6 +136,7 @@ const Cart = ({ cart, updateQuantity, toggleCart, clearCart }) => {
 };
 
 export default Cart;
+
 
 
 
